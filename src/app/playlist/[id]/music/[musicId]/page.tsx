8deff5a180b5414b3012bb41f86af9bfe6d4c2f5
Luciano Paranhos <luciano@paranhos.com.br>
2025-05-07 08:51:26 -0300

    feat(player): adiciona breadcrumb e estilização refinada à página de música
    Implementa navegação tipo breadcrumb na página de player com links para a página inicial e para a lista de playlists.
    Estilização inspirada no design da Apple, com fonte aprimorada e alinhamento à esquerda sem afetar os estilos globais.

--- src/app/playlist/[id]/music/[musicId]/page.tsx
+++ src/app/playlist/[id]/music/[musicId]/page.tsx
@@ -1,19 +1,37 @@
+import Link from "next/link";
 import styles from "./page.module.css";
 
 export default function Home() {
     return (
         <main className="main">
-            <section className="card">
+            <section className={`card ${styles.breadcrumbContainer}`}>
                 <header>
-                    <h1 className={styles.title}>
-                        Nome da música
-                    </h1>
+                    <nav className={styles.breadcrumb}>
+                        <Link href="/" className={styles.breadcrumbLink}>
+                            🏠 Início
+                        </Link>
+                        <span className={styles.separator}>/</span>
+                        <Link href="/playlist/algum-Id" className={styles.breadcrumbLink}>
+                            Playlist
+                        </Link>
+                    </nav>
                 </header>
+            </section>
+            <br />
+            <section className="card">
                 <section>
                     <iframe width="420" height="315"
                         src="https://www.youtube.com/embed/tgbNymZ7vqY">
                     </iframe>
                 </section>
+            </section>
+            <br />
+            <section className="card">
+                <header>
+                    <h1 className={styles.title}>
+                        controlers
+                    </h1>
+                </header>
             </section>
         </main>
     );
